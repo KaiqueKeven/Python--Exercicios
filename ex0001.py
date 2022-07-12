007b12541c0270a6d3cc9b4091011f1ccd079d10
Kaique_Keven <garouzoro@gmail.com>
2022-07-11 22:41:55 -0300

    ex 1

---
+++ ex0001.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um numero inteiro
+# e mostre na tela o seu sussesor e seu antecessor 
+numero = int(input('Digite um valor: '))
+print(f'O seu sussessor e {numero + 1} e o antecessor {numero - 1}')
