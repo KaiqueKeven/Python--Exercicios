54df4a7cf1c85b8797c3d3f809968c0ab7994e1d
Kaique_Keven <garouzoro@gmail.com>
2022-07-11 22:58:21 -0300

    ex2

---
+++ Exercícios/ex002.py
@@ -0,0 +1,5 @@
+# crie um algoritimo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+numero = int(input('Digite um valor: '))
+print(f'O dobro de {numero} e {numero*2}')
+print(f'O triplo de {numero} e {numero*3}')
+print(f'A raiz quadrada {numero} e {numero**(1/2)}')
