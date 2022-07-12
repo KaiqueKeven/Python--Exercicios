8d3c0944a960eef5f1108ded759b94c54497a4a6
Kaique_Keven <garouzoro@gmail.com>
2022-07-11 23:13:59 -0300

    ex3

---
+++ Exercícios/ex003.py
@@ -0,0 +1,5 @@
+#desenvolva um programa que leia as duas notas de um aluno, calcule e mostrea sua média 
+nota1 = float(input('Digite sua nota: '))
+nota2 = float(input('Digite outra nota: '))
+media = (nota1 + nota2) / 2 
+print(f'A média do aluno e {media}')
